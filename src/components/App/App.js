90412a572b80876f7f687b020ad73cb205b653e6
Joshua Comeau <joshwcomeau@gmail.com>
2015-02-14 18:15:41 -0500

    Cloned react-starter-kit. Removed all the assets, styles and react components I don't need.

---
+++ src/components/App/App.js
@@ -0,0 +1,52 @@
+/*
+ * React.js Starter Kit
+ * Copyright (c) 2014 Konstantin Tarkus (@koistya), KriaSoft LLC.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.txt file in the root directory of this source tree.
+ */
+
+'use strict';
+
+import './App.less';
+
+import React from 'react';
+import invariant from 'react/lib/invariant';
+import AppActions from '../../actions/AppActions';
+import NavigationMixin from './NavigationMixin';
+import AppStore from '../../stores/AppStore';
+import NotFoundPage from '../NotFoundPage';
+
+var Application = React.createClass({
+
+  mixins: [NavigationMixin],
+
+  propTypes: {
+    path: React.PropTypes.string.isRequired,
+    onSetTitle: React.PropTypes.func.isRequired,
+    onSetMeta: React.PropTypes.func.isRequired,
+    onPageNotFound: React.PropTypes.func.isRequired
+  },
+
+  render() {
+    var page = AppStore.getPage(this.props.path);
+    invariant(page !== undefined, 'Failed to load page content.');
+    this.props.onSetTitle(page.title);
+
+    if (page.type === 'notfound') {
+      this.props.onPageNotFound();
+      return React.createElement(NotFoundPage, page);
+    }
+
+    return (
+      /* jshint ignore:start */
+      <div className="App">
+        <h2>Hi!</h2>
+      </div>
+      /* jshint ignore:end */
+    );
+  }
+
+});
+
+module.exports = Application;
